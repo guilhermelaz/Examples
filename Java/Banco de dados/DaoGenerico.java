14b9aa051408c67ef88c008e9f34ca6a33ede64e
Guilherme Lazarotto <105259017+guilhermelaz@users.noreply.github.com>
2023-06-28 20:01:42 -0300

    update DaoGenerico
    DaoGenerico do professor

--- Java/Banco de dados/DaoGenerico.java
+++ Java/Banco de dados/DaoGenerico.java
@@ -1,8 +1,13 @@
-import java.sql.Statement;
+package model;
+
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class DaoGenerico {
     private Connection conn;
@@ -10,102 +15,325 @@ public class DaoGenerico {
     private final String TB_PREFIX = "tb_";
     private final String TB_SUFFIX = "s";
 
-
-    private void conectar() {
+    
+    private void conectar(){
         try {
-            this.conn = GerenciarConexao.pegarConexao();
+            this.conn = GerenciadorConexao.pegarConexao();
             st = conn.createStatement();
         } catch (ClassNotFoundException e1) {
             System.out.println(e1.getMessage());
-        } catch (SQLException e2) {
+        }catch (SQLException e2) {
             System.out.println(e2.getMessage());
         }
     }
 
-    private void desconectar() {
+    private void desconectar(){
         try {
             st.close();
             conn.close();
-        } catch (SQLException e2) {
-            System.out.println(e2.getMessage());
-        }
-    }
+        }catch (SQLException e) {
+            System.out.println("Erro ao fechar a conexao: " 
+                + e.getMessage());
+        }        
+    }    
 
-
-    public void inserir (Object o) throws Exception {
+    public void inserir(Object o) throws Exception{
         Class c = o.getClass();
         Field fields[] = c.getDeclaredFields();
 
-        // Montar o comando
-        String comando = "INSERT INTO " + TB_PREFIX + c.getSimpleName().toLowerCase() 
-            + TB_SUFFIX;
-
+        //montar a query para o pst
+        String query = "INSERT INTO " + TB_PREFIX 
+            + c.getSimpleName().toLowerCase() + TB_SUFFIX;
         String campos = "(";
-        String valores = "VALUES(";
+        String aliases = " VALUES(";
         boolean separar = false;
-        
         for (Field f : fields) {
-            if (separar) {
+            if(separar){
                 campos = campos + ", ";
-                valores = valores + ", ";
+                aliases = aliases + ", ";
             }
             campos = campos + f.getName();
-            valores = valores + "?";
+            aliases = aliases + "?";
             separar = true;
-
         }
-
         campos = campos + ") ";
-        valores = valores + ") ";
+        aliases = aliases + ") ";
+        query = query + campos + aliases;
 
-        comando = comando + campos + valores;
-        System.out.println(comando);
+        // System.out.println(campos);
+        // System.out.println(aliases);
+        System.out.println("- - - > " + query);
 
-        // Criar statement e setar valores nos parametros
+
+        //criar o pst e setar os valores
         this.conectar();
-        PreparedStatement pst = conn.prepareStatement(comando, Statement.RETURN_GENERATED_KEYS);
-        
-        int numeroParametro = 0;
+        PreparedStatement pst = conn.prepareStatement(
+            query, Statement.RETURN_GENERATED_KEYS);        
+
+        //seta os valores dos params
+        int numParam = 0;
         for (Field f : fields) {
-            numeroParametro++;
-
+            numParam++;
             f.setAccessible(true);
-
+            System.out.println("tipo: " + f.getType());
             if(f.getType().isAssignableFrom(String.class)){
-                System.out.println("Tipo: " + f.getType());
-                if (f.get(o) != null){
-                    pst.setString(numeroParametro, f.get(o).toString()); 
-                } else {
-                    pst.setString(numeroParametro, "");
+                if(f.get(o) != null){
+                    pst.setString(numParam, f.get(o).toString());
+                }else{
+                    pst.setString(numParam, "");
                 }
-            } 
-            
-            else if (f.getType().isAssignableFrom(Integer.class) || f.getType().isAssignableFrom(Integer.TYPE)) {
-                System.out.println("Tipo: " + f.getType());
-                if (f.get(o) != null){
-                    pst.setInt(numeroParametro, Integer.parseInt(f.get(o).toString()));
-                } else {
-                    pst.setInt(numeroParametro, 0);
-                }
-            }
-            
-            else if (f.getType().isAssignableFrom(Double.class) || f.getType().isAssignableFrom(Double.TYPE)) {
-                System.out.println("Tipo: " + f.getType());
-                if (f.get(o) != null){
-                    pst.setDouble(numeroParametro, Double.parseDouble(f.get(o).toString()));
-                } else {
-                    pst.setDouble(numeroParametro, 0);
+            }else if(f.getType().isAssignableFrom(Integer.class)
+                    || f.getType().isAssignableFrom(Integer.TYPE)){
+                pst.setInt(numParam, 
+                    Integer.parseInt(f.get(o).toString()));
+            }else if(f.getType().isAssignableFrom(Float.class)
+                    || f.getType().isAssignableFrom(Float.TYPE)){
+                
+                pst.setFloat(numParam, 
+                    Float.parseFloat(f.get(o).toString()));
+            }else if(f.getType().isAssignableFrom(LocalDate.class)){
+                
+                if(f.get(o) != null){
+                    pst.setDate(numParam, 
+                        java.sql.Date.valueOf(f.get(o).toString()));
+                }else{
+                    pst.setDate(numParam, null);
                 }
             }
-            
         }
         pst.executeUpdate();
         this.desconectar();
-
-        System.out.println("Inserido!");
     }
-}
+
+    public Object consultar(Object o, String chave, int id) throws Exception{
+        Class c = o.getClass();
+        Field fields[] = c.getDeclaredFields();
+        Object objRet = c.getConstructor().newInstance();
+        try {
+            this.conectar();
+            String query = "SELECT * FROM " + TB_PREFIX 
+                + c.getSimpleName().toLowerCase() + TB_SUFFIX
+                + " WHERE " + chave + " = " + id + ";";
+            // System.out.println(query);
+            // System.exit(9);
+            ResultSet rs = st.executeQuery(query);
+            if(rs.next()){
+                for (Field f : fields){
+                    f.setAccessible(true); 
+                    if(f.getType().isAssignableFrom(String.class)){
+        
+                        f.set(objRet, rs.getString(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Integer.class)
+                            || f.getType().isAssignableFrom(Integer.TYPE)){
+                        
+                        f.set(objRet, rs.getInt(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Float.class)
+                    || f.getType().isAssignableFrom(Float.TYPE)){
+                        
+                        f.set(objRet, rs.getFloat(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(LocalDate.class)){
+                        
+                        f.set(objRet, rs.getDate(
+                            f.getName().toString()).toLocalDate());
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            desconectar();
+        }
+        return objRet;
+    }   
+
+    public ArrayList<Object> buscarTodos(Object o) throws Exception{
+        Class c = o.getClass();
+        Field fields[] = c.getDeclaredFields();
+        ArrayList<Object> resultados = new ArrayList<Object>();
+        try {
+            this.conectar();
+            String query = "SELECT * FROM " + TB_PREFIX 
+                + c.getSimpleName().toLowerCase() + TB_SUFFIX + ";";
+            // System.out.println(query);
+            // System.exit(9);
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                Object objRet = c.getConstructor().newInstance();
+                for (Field f : fields){
+                    f.setAccessible(true); 
+                    if(f.getType().isAssignableFrom(String.class)){
+        
+                        f.set(objRet, rs.getString(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Integer.class)
+                            || f.getType().isAssignableFrom(Integer.TYPE)){
+                        
+                        f.set(objRet, rs.getInt(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Float.class)
+                    || f.getType().isAssignableFrom(Float.TYPE)){
+                        
+                        f.set(objRet, rs.getFloat(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(LocalDate.class)){
+                        
+                        f.set(objRet, rs.getDate(
+                            f.getName().toString()).toLocalDate());
+                    }
+                }
+                resultados.add(objRet);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            desconectar();
+        }
+        return resultados;
+    }   
+
+    public ArrayList<Object> buscarTodosFiltro(
+            Object o, String campo, String filtro) throws Exception{
+        Class c = o.getClass();
+        Field fields[] = c.getDeclaredFields();
+        ArrayList<Object> resultados = new ArrayList<Object>();
+        try {
+            this.conectar();
+            String query = "SELECT * FROM " + TB_PREFIX 
+                + c.getSimpleName().toLowerCase() + TB_SUFFIX 
+                + " WHERE " + campo + " LIKE '%" + filtro 
+                + "%' ORDER BY " + campo + ";";
+            // System.out.println(query);
+            // System.exit(9);
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                Object objRet = c.getConstructor().newInstance();
+                for (Field f : fields){
+                    f.setAccessible(true); 
+                    if(f.getType().isAssignableFrom(String.class)){
+        
+                        f.set(objRet, rs.getString(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Integer.class)
+                            || f.getType().isAssignableFrom(Integer.TYPE)){
+                        
+                        f.set(objRet, rs.getInt(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Float.class)
+                    || f.getType().isAssignableFrom(Float.TYPE)){
+                        
+                        f.set(objRet, rs.getFloat(f.getName().toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(LocalDate.class)){
+                        
+                        f.set(objRet, rs.getDate(
+                            f.getName().toString()).toLocalDate());
+                    }
+                }
+                resultados.add(objRet);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            desconectar();
+        }
+        return resultados;
+    }   
     
+    public int excluir(Object o, String chave, int valor){
+        Class c = o.getClass();
+        int qtde = 0;
+        try {
+            this.conectar();
+            String comando = "DELETE FROM " + TB_PREFIX 
+                + c.getSimpleName().toLowerCase() + TB_SUFFIX 
+                + " WHERE " + chave + " = " + valor +";";
+            st.executeUpdate(comando);
+            qtde = st.getUpdateCount();
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }finally{
+            this.desconectar();
+        }
+        return qtde;
+    }
+    
+    public int alterar(Object o, String chave, int id) throws Exception{
+        Class c = o.getClass();
+        Field fields[] = c.getDeclaredFields();
+        
+        //monta a query para o pst
+        String query = "UPDATE " + TB_PREFIX + c.getSimpleName().toLowerCase() + "s SET ";
+        boolean separar = false;
+        for (Field f : fields){
 
+            if(!f.getName().equals(chave)){//não pode tentar atualizar o id
 
+                if(separar){
+                    query = query + ", ";
+                }
+                separar = true;
+                query = query + f.getName() + " = ?";
+            }
+        }
+        query = query + " WHERE " + chave + " = " + id + ";";
+        System.out.println("- - - - - > " + query);
+        // System.exit(9);
 
+        //cria o pst
+        int qtde = 0;
+		try {
+            this.conectar();
+            PreparedStatement pst = conn.prepareStatement(
+                query,Statement.RETURN_GENERATED_KEYS);
+
+            //seta os valores dos params
+            int numParam = 0;
+            for (Field f : fields){
+                if(!f.getName().equals(chave)){//não pode tentar atualizar o id
+
+                    numParam++;
+                    f.setAccessible(true); 
+                    if(f.getType().isAssignableFrom(String.class)){
+
+                        if(f.get(o) != null){
+                            pst.setString(numParam, f.get(o).toString());
+                        }else{
+                            pst.setString(numParam, "");
+                        }
+                        
+                    }else if(f.getType().isAssignableFrom(Integer.class)
+                            || f.getType().isAssignableFrom(Integer.TYPE)){
+                        
+                        pst.setInt(numParam, Integer.parseInt(f.get(o).toString()));
+                        
+                    }else if(f.getType().isAssignableFrom(Float.class)
+                            || f.getType().isAssignableFrom(Float.TYPE)){
+                        
+                        pst.setFloat(numParam, Float.parseFloat(f.get(o).toString()));
+
+                    }else if(f.getType().isAssignableFrom(LocalDate.class)){
+                        
+                        if(f.get(o) != null){
+                            pst.setDate(numParam, java.sql.Date.valueOf(f.get(o).toString()));
+                        }else{
+                            pst.setDate(numParam, null);
+                        }
+                    }
+                }
+            }
+            pst.executeUpdate();
+            qtde = st.getUpdateCount();
+
+        } catch (SQLException e) {  
+            System.out.println("Erro ao atualizar:" + e.getMessage());  
+		} catch (Exception e) {  
+            System.out.println("Erro ao atualizar:" + e.getMessage());  
+		} finally {  
+            this.desconectar();
+		}  
+        return qtde;   
+	}
+}
